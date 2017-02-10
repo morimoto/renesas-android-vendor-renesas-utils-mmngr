f03a8b9b9ce6452a18000ab3d8819a80cb8e2b69
Maksym Kreshchuk <maksym.kreshchuk@globallogic.com>
2017-02-10 19:02:21 +0200

    Android.mk: Add include dir from mmngr_km
    Change-Id: I03e85c30dcf4e737e246d40ef08aa052d23453d8
    Signed-off-by: Maksym Kreshchuk <maksym.kreshchuk@globallogic.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,8 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 
 LOCAL_SHARED_LIBRARIES := liblog
 
-LOCAL_C_INCLUDES:= $(LOCAL_PATH)/libmmngr/mmngr/include
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/libmmngr/mmngr/include \
+hardware/renesas/mmngr_km/mmngr_drv/mmngr/mmngr-module/files/mmngr/include
 
 LOCAL_SRC_FILES := libmmngr/mmngr/if/mmngr_if.c
 
